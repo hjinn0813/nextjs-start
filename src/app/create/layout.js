b3a550adf8482d0b31f8c2c1d5603978a03095d8
hjinn0813 <hjc3790@gmail.com>
2024-08-21 17:38:41 +0900

    feat: 8.21일 학습 - next.js 글 생성

--- src/app/create/layout.js
+++ src/app/create/layout.js
@@ -2,10 +2,10 @@
 
 export default function Layout(props) {
   return (
-    <form>
+    <>
       <h2>Create</h2>
       {props.children}
       {/* children은 create/page.js에서 왔음 */}
-    </form>
+    </>
   );
 }
--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,5 +1,42 @@
 // create page
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 export default function Create() {
-  return <>Let us Create!!</>;
+  const router = useRouter();
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const lastid = result.id;
+            router.push(`read/${lastid}`); // 생성된 페이지로 리디렉션
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
