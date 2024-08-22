aa278d395f1a8cda870c7d5c44b7b3305baa15ef
hjinn0813 <hjc3790@gmail.com>
2024-08-22 14:13:19 +0900

    feat: 8월 22일 학습 - next.js update 기능 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,9 @@
 // read page
 
 export default async function Read(props) {
-  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`, {
+    cache: 'no-store',
+  });
   const topic = await resp.json();
 
   return (
---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,69 @@
+// update id
+
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function Update() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+
+  useEffect(() => {
+    fetch('http://localhost:9999/topics/' + id)
+      .then((resp) => resp.json())
+      .then((result) => {
+        setTitle(result.title);
+        setBody(result.body);
+      });
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics/` + id, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const lastid = result.id;
+            router.refresh(); // comp refresh, 데이터 변경 반영을 위한 강제 렌더링
+            router.push(`read/${lastid}`); // 생성된 페이지로 리디렉션
+          });
+      }}
+    >
+      <p>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </p>
+      <p>
+        <textarea
+          name="body"
+          placeholder="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </p>
+      <p>
+        <input type="submit" value="update" />
+      </p>
+    </form>
+  );
+}
