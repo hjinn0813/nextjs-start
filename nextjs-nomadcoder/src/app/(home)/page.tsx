856bcb46412298fb274801898fd5d534656c90c0
hjinn0813 <hjc3790@gmail.com>
2024-08-27 12:34:46 +0900

    feat: next.js data fetching

--- nextjs-nomadcoder/src/app/(home)/page.tsx
+++ nextjs-nomadcoder/src/app/(home)/page.tsx
@@ -4,10 +4,17 @@ export const metadata = {
   title: 'Home',
 };
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovie() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const resp = await fetch(URL);
+  const json = await resp.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovie();
+
+  return <div>{JSON.stringify(movies)}</div>;
 }
