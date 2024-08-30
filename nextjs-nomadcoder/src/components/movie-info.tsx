dbb344d6c5ef6dd9cdbb0bd3a688fbe246eff6e2
hjinn0813 <hjc3790@gmail.com>
2024-08-30 18:07:09 +0900

    feat: next.js movie info comps design

--- nextjs-nomadcoder/src/components/movie-info.tsx
+++ nextjs-nomadcoder/src/components/movie-info.tsx
@@ -1,14 +1,28 @@
 // movie info comp
 
 import { API_URL } from '../app/(home)/page';
+import style from '../styles/movie-info.module.css';
 
 async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const resp = await fetch(`${API_URL}/${id}`);
   return resp.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={style.container}>
+      <img className={style.poster} src={movie.poster_path} alt={movie.title} />
+      <div className={style.info}>
+        <h1 className={style.title}>{movie.title}</h1>
+        <h3>⭐ {movie.vote_average.toFixed(1)}</h3>
+        <p>🎬 Release Date : {movie.release_date}</p>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={'_blank'}>
+          HOMEPAGE →
+        </a>
+      </div>
+    </div>
+  );
 }
