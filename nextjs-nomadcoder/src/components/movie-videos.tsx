dd2f3fc976efdbfa3b13f891232c04019b3bb09c
hjinn0813 <hjc3790@gmail.com>
2024-08-30 18:19:16 +0900

    feat: next.js movie video design

--- nextjs-nomadcoder/src/components/movie-videos.tsx
+++ nextjs-nomadcoder/src/components/movie-videos.tsx
@@ -1,14 +1,27 @@
 // video component
 
 import { API_URL } from '../app/(home)/page';
+import style from '../styles/movie-video.module.css';
 
 async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const resp = await fetch(`${API_URL}/${id}/videos`);
   return resp.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={style.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
